fbac5302a3d185506122ab416387f6ebae57170f
946336 <wesleywei@protonmail.com>
2017-10-18 15:33:21 -0400

    fixed sample function name

--- sample_examples.cpp
+++ sample_examples.cpp
@@ -5,24 +5,24 @@
 
 #include <stdio.h>
 
-int fib(int n)
+int fact(int n)
 {
     if (n == 0) {
         return 1;
     }
-    return n * fib(n- 1);
+    return n * fact(n- 1);
 }
 
 int main()
 {
-    auto fib_memoized = memoize<Hashable<int(int)>>(fib);
+    auto fact_memoized = memoize<Hashable<int(int)>>(fact);
 
     for (int i = 0; i < 20; ++i) {
-        fib_memoized(i);
+        fact_memoized(i);
     }
 
     for (int i = 0; i < 20; ++i) {
-        fib_memoized(i);
+        fact_memoized(i);
     }
 
     return 0;
